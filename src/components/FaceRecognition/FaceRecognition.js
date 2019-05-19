1ee2f1f362d8d92ae0db7ee9db84add1fd1eaa70
Rhythm <rrhythmsharma@gmail.com>
2019-05-20 00:14:56 +0530

    FaceRecognition: Add box to face detected by API

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
     return(
         <div className="center ma">
             <div className='absolute mt2'>
                 <img id='inputimage' alt={'photo'} src={imageUrl} width="500px" height="auto" />
+                <div className='bounding-box' style={{top: box.toprow, right: box.rightcol, bottom: box.bottomrow, left: box.leftcol}} >
+                </div>
             </div>
         </div>
     );
