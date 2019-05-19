9f36806d3c9956e4ed5b4e4657658f50a0ab632a
Rhythm <rrhythmsharma@gmail.com>
2019-05-19 13:18:48 +0530

    ImageLinkForm: add imagelink section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 import 'tachyons';
 
@@ -10,8 +11,8 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
-        {/*<ImageLinkForm />
-        <FaceRecognition />*/}
+        <ImageLinkForm />
+        {/*<FaceRecognition />*/}
       </div> 
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Tilt from 'react-tilt'
+import './Logo.css';
+import signin from './sign-in.png';
 
 const Logo = () => {
     return(
         <div className="ma4 mt0">
-        </div>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner">
+                    <img style={{paddingTop: '5px'}} alt="logo" src={signin} />
+                </div>
+            </Tilt>
+        </div>        
     );
 }
 
-export default Logo;  
+export default Logo; 
