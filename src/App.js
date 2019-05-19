469164ea637fc4a117576c8d68c7a3301834cc64
Rhythm <rrhythmsharma@gmail.com>
2019-05-19 23:51:58 +0530

    Clarifai: add clarifai api & FaceRecognition: add functionality of recognitoin

--- src/App.js
+++ src/App.js
@@ -1,20 +1,87 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 import 'tachyons';
 
+const app = new Clarifai.App({
+  apiKey: '232a5948f1ab40f58bc8e21ec1190b73'
+ });
+ 
+
+const particleprams = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftcol: clarifaiFace.left_col * width,
+      toprow: clarifaiFace.top_row * height,
+      rightcol: width - (clarifaiFace.right_col * width),
+      bottomrow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.
+      predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))//response.outputs[0].data.regions[0].region_info.bounding_box
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
+        <Particles 
+          className="particles"
+          params={particleprams}  />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {<FaceRecognition imageUrl={this.state.imageUrl}/>}
       </div> 
     );
   }
