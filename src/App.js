2d2a442dc7e452eb8f5a85bb419df07b40295520
Rhythm <rrhythmsharma@gmail.com>
2019-05-20 16:54:52 +0530

    Front-end work Completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import './App.css';
 import 'tachyons';
 
@@ -34,7 +35,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'SignIn'
+      route: 'SignIn',
+      isSignedIn: false
     }
   }
 
@@ -65,12 +67,17 @@ class App extends Component {
     app.models.predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))//response.outputs[0].data.regions[0].region_info.bounding_box
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
       .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
-    this.setState({route: route})
+    if(route === 'signout') {
+      this.setState({isSignedIn: false})
+    }else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
   }
 
   render() {
@@ -79,19 +86,24 @@ class App extends Component {
         <Particles 
           className="particles"
           params={particleprams}  />
-        <Navigation onRouteChange={this.onRouteChange}/> 
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/> 
         {
-          this.state.route === "SignIn"
-           ? <SignIn onRouteChange={this.onRouteChange}/>
-           : <div>
-              <Logo />
-              <Rank />
-              <ImageLinkForm 
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
-             </div>
+          this.state.route === "home"
+           ? <div>
+                <Logo />
+                <Rank />
+                <ImageLinkForm 
+                  onInputChange={this.onInputChange}
+                  onButtonSubmit={this.onButtonSubmit}
+                />
+                <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+              </div>
+            : (
+                this.state.route === "SignIn"
+                ? <SignIn onRouteChange={this.onRouteChange}/>
+                : <Register onRouteChange={this.onRouteChange}/>
+              )
+           
         }
       </div> 
     );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import './SignIn.css';
 
-const SignIn = ({onRouteChange}) => {
+const Register = ({onRouteChange}) => {
     return(
-        <div>
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                        <legend className="f1 fw6 ph0 mh0">Register</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+                        </div>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
@@ -22,17 +24,13 @@ const SignIn = ({onRouteChange}) => {
                         <input 
                             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                             type="submit" 
-                            value="Sign in"
+                            value="Register"
                             onClick={ () => onRouteChange('home')} />
                     </div>
-                    <div className="lh-copy mt3">
-                        <p onClick={ () => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-                    </div>
-                </form>
+                </div>
             </main>
-            </article>
-        </div>
+        </article>
     );
 }
 
-export default SignIn;
+export default Register;
