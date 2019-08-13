e15f1fbb63d5f8cd88547d7c5a6e7f50affe61f7
Rhythm <rrhythmsharma@gmail.com>
2019-08-13 17:08:32 +0530

    Add new design to navbar links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
                 return(
                     <nav className="flex justify-between bb b--white-10 shadow-3 navbar-container"  style = {{display: 'flex', justifyContent: 'flex-end'}}>
                         <div className="flex-grow flex items-center">
-                            <p onClick={ () => onRouteChange('signout')} className="f6 dib black bg-animate hover-bg-black hover-white mr3 mr4-ns pv2 ph2 pointer navbar-button">Sign out</p>
+                            <p onClick={ () => onRouteChange('signout')} className="f6 dib white bg-black mr3 mr4-ns pv2 ph2 pointer navbar-button">Sign out</p>
                         </div>
                     </nav>
                 );
@@ -14,8 +14,8 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
                 return(
                     <nav className="flex justify-between bb b--white-10 shadow-3 navbar-container"  style = {{display: 'flex', justifyContent: 'flex-end'}}>
                         <div className="flex-grow flex items-center">
-                            <p onClick={ () => onRouteChange('SignIn')}  className="f6 dib black bg-animate hover-bg-black hover-white mr1 mr4-ns pv2 ph2 br-pill pointer navbar-button">Sign in</p>
-                            <p onClick={ () => onRouteChange('Register')} className="f6 dib black bg-animate hover-bg-black hover-white mr1 mr4-ns pv2 ph2 br-pill pointer navbar-button">Register</p>
+                            <p onClick={ () => onRouteChange('SignIn')}  className="f6 dib white bg-black mr1 mr4-ns pv2 ph2 br-pill pointer navbar-button">Sign in</p>
+                            <p onClick={ () => onRouteChange('Register')} className="f6 dib white bg-black mr1 mr4-ns pv2 ph2 br-pill pointer navbar-button">Register</p>
                         </div>
                     </nav>
                 );
